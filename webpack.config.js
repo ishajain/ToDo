395bf3f75dd9b825a4416b4ed011cd7a84e58939
Isha Jain <isha.jain30@gmail.com>
2019-03-07 09:44:04 +1100

    Added Semantic UI styling

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,29 @@
 const path = require("path");
 const webpack = require("webpack");
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+
 
 module.exports = {
     mode: 'development',
     entry: {
       main:"./src/index.js",
-      vendor: ["react","redux",'react-redux']
+      vendor: ["react","react-dom","redux",'react-redux']
     },
     output: {
       path: path.resolve(__dirname, "dist/"),
       publicPath: "/dist/",
       filename: '[name].bundle.js',
     },
+    optimization: {
+    
+    splitChunks: {
+      cacheGroups: {
+        vendors: {
+          filename: '[name].bundle.js'
+        }
+      }
+    }},
     devServer: {
       contentBase: path.join(__dirname, "public/"),
       port: 8080,
@@ -29,9 +41,22 @@ module.exports = {
       {
         test: /\.css$/,
         use: ["style-loader", "css-loader"]
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: "html-loader",
+            options: { minimize: true }
+          }
+        ]
       }
     ]
   },
   resolve: { extensions: ["*", ".js", ".jsx"] },
-  plugins: [new webpack.HotModuleReplacementPlugin()]
+  plugins: [new webpack.HotModuleReplacementPlugin(),
+            new HtmlWebPackPlugin
+            (),
+            new CleanWebpackPlugin(),
+  ]
 };
